633c78d2a7c754ebae9e9c5ad43c8dfbd2c840c3
Angela Naseri <104205840+angnas@users.noreply.github.com>
2024-08-18 11:22:03 +0200

    add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Button, TextField, Box, Callout, Text } from "@radix-ui/themes";
+import {
+  Button,
+  TextField,
+  Box,
+  Callout,
+  Text,
+  Spinner,
+} from "@radix-ui/themes";
 import SimpleMdeReact from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
@@ -26,6 +33,7 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
 
   return (
     <div className="max-w-xl">
@@ -37,9 +45,11 @@ const NewIssuePage = () => {
       <form
         onSubmit={handleSubmit(async (data) => {
           try {
+            setSubmitting(true);
             await axios.post("/api/issues", data);
             router.push("./issues");
           } catch (error) {
+            setSubmitting(false);
             setError("An unexpected error occurred!");
           }
         })}
@@ -59,7 +69,9 @@ const NewIssuePage = () => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
